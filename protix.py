61b620738e338a98324e463ea5ae4a18915a011d
V <why@s.com>
2021-08-05 01:01:07 +0200

    added docstrings, simplified solution

--- protix.py
+++ protix.py
@@ -20,6 +20,7 @@
 # assigned the second bike.
 
 from typing import List
+import sys
 
 
 class Position:
@@ -91,18 +92,23 @@ class BikeLease:
 
     def assign_bikes(self) -> None:
         """
-        Assign the bikes to each user in a first come first serve basis
+        Assign the bikes to each user on a first come first serve basis
+        for each user:
+            for each bike:
+                distances-> manhattan(user, bike)
+            return bike with min(distances)
+            remove current bike from bikelist
         """
         for user_pos in self.list_of_user_positions:
-            min_distance = 0
-            closest_bike = None
-            for bike_pos in self.list_of_bike_positions:
+            min_distance = sys.maxsize * 2 + 1  # set minimum distance to largest value possible , sys.maxsize
+            closest_bike_index = None
+            for i, bike_pos in enumerate(self.list_of_bike_positions):
                 distance = self.calculate_manhattan_distance(user_pos=user_pos, bike_pos=bike_pos)
-                if distance < min_distance or min_distance == 0:
+                if distance < min_distance:
                     min_distance = distance
-                    closest_bike = bike_pos
+                    closest_bike_index = i
+            closest_bike = self.list_of_bike_positions.pop(closest_bike_index)
             self.assigned_bikes.append((user_pos, closest_bike))
-            self.list_of_bike_positions.remove(closest_bike)  # remove bike from available bikes
 
 
 def run_test(users: List[tuple], bikes: List[tuple], test_answer: List[tuple]) -> None:
@@ -122,6 +128,17 @@ def run_test(users: List[tuple], bikes: List[tuple], test_answer: List[tuple]) -
 
 
 if __name__ == "__main__":
+
+    # xx denotes an empty space
+    # BK denotes a bike
+    # Ui denotes User i
+
+    #############################################
+
+    #  xx xx BK
+    #  xx U2 BK
+    #  U1 BK U3
+
     users_pos = [(0, 0), (1, 1), (2, 0)]
     bikes_pos = [(1, 0), (2, 2), (2, 1)]
     test_ans = [((0, 0), (1, 0)), ((1, 1), (2, 1)), ((2, 0), (2, 2))]
@@ -130,14 +147,47 @@ if __name__ == "__main__":
 
     print('test 1 correct')
 
-    users_pos = [(0, 0), (1, 1), (3, 3), (5, 0), (6, 11)]
-    bikes_pos = [(1, 0), (2, 2), (2, 1), (6, 6), (10, 10), (2, 3)]
-    test_ans = [((0, 0), (1, 0)),
-                ((1, 1), (2, 1)),
-                ((3, 3), (2, 3)),
-                ((5, 0), (2, 2)),
-                ((6, 11), (6, 6))]
+    #############################################
+
+    # 5  xx xx U3 xx xx BK
+    # 4  xx BK xx U2 xx xx
+    # 3  xx xx BK xx U6 xx
+    # 2  xx BK xx xx xx U5
+    # 1  xx U4 xx BK xx xx
+    # 0  U1 xx xx xx BK xx
+    #    0  1  2  3  4  5
+
+    users_pos = [(0, 0), (3, 4), (2, 5), (1, 1), (5, 2), (4, 3)]
+    bikes_pos = [(1, 2), (4, 0), (2, 3), (5, 5), (1, 4), (3, 1)]
+
+    test_ans = [((0, 0), (1, 2)),
+                ((3, 4), (2, 3)),
+                ((2, 5), (1, 4)),
+                ((1, 1), (3, 1)),
+                ((5, 2), (4, 0)),
+                ((4, 3), (5, 5))]
 
     run_test(users_pos, bikes_pos, test_ans)
     print('test 2 correct')
+
+    #############################################
+
+    # 7  xx xx xx xx xx xx xx xx
+    # 6  xx xx xx xx xx xx xx xx
+    # 5  xx xx U2 xx xx xx xx xx
+    # 4  xx BK xx xx xx xx xx xx
+    # 3  xx xx xx xx xx xx xx xx
+    # 2  xx xx xx xx xx xx xx U1
+    # 1  BK U3 xx xx xx xx xx xx
+    # 0  BK xx xx xx xx xx xx xx
+    #    0  1  2  3  4  5  6  7
+
+    users_pos = [(7, 2), (2, 5), (1, 1)]
+    bikes_pos = [(0, 0), (1, 4), (0, 1)]
+    test_ans = [((7, 2), (1, 4)),
+                ((2, 5), (0, 1)),
+                ((1, 1), (0, 0))]
+
+    run_test(users_pos, bikes_pos, test_ans)
+    print('test 3 correct')
 
